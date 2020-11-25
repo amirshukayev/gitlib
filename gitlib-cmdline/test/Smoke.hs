d576a95938284ac9c8850b0ca9bcc1386d9d17b1
Amir Shukayev <amir.shukayev@gmail.com>
2020-11-24 17:27:12 -0700

    removing unecessary projects like cmdline and libgit2

--- gitlib-cmdline/test/Smoke.hs
+++
@@ -1,16 +0,0 @@
-{-# LANGUAGE OverloadedStrings #-}
-{-# OPTIONS_GHC -fno-warn-unused-do-bind #-}
-{-# OPTIONS_GHC -fno-warn-wrong-do-bind #-}
-{-# OPTIONS_GHC -fno-warn-name-shadowing #-}
-
-module Main where
-
-import qualified Git.CmdLine as Cli
-import qualified Git.Smoke as Git
-import           Test.Hspec.HUnit ()
-import           Test.Hspec.Runner
-
-main :: IO ()
-main = hspec $ Git.smokeTestSpec Cli.cliFactory Cli.cliFactory
-
--- Smoke.hs ends here
